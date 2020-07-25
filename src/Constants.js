12c61506269fa869a6d1ba540e31375ffad3cb6b
JoaquinTR <joaquintricerri@gmail.com>
2020-07-25 00:42:36 -0300

    Linkeado Laravel en Heroku.

--- src/Constants.js
+++ src/Constants.js
@@ -3,6 +3,6 @@
     que se comparta a lo largo de la SPA. Útil al intercambiar entre una dirección de API local a
     una remota.
 */
-export const APIurl = "http://127.0.0.1:8000";
+export const APIurl = "https://gaming-place.herokuapp.com";
 
-export const GamingPlace = "http://localhost:8000/"
+export const GamingPlace = "https://gaming-place.herokuapp.com";
